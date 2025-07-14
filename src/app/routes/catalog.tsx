48098915c95e03feaf8f32c977c4fda392b2ebda
iiivanpopov <iiivanpopov999@gmail.com>
2025-07-14 23:30:20 +0300

    feat: pagination

--- src/app/routes/catalog.tsx
+++ src/app/routes/catalog.tsx
@@ -1,14 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router'
+import z from 'zod'
 import { getSneakerBrands, getSneakers } from '@/shared/api'
 import { ROUTES } from '@/shared/constants/routes'
 
 export const Route = createFileRoute(ROUTES.CATALOG)({
-  loader: async () => {
+  loader: async ({ location }) => {
     try {
       const [sneakers, brands] = await Promise.all([
         getSneakers({
           params: {
-            limit: '10'
+            limit: '10',
+            offset:
+              'page' in location.search
+                ? String(Number(location.search.page) * 10)
+                : '0'
           }
         }),
         getSneakerBrands({})
@@ -22,5 +27,8 @@ export const Route = createFileRoute(ROUTES.CATALOG)({
       console.error(error)
       return { sneakers: [], brands: [] }
     }
-  }
+  },
+  validateSearch: z.object({
+    page: z.coerce.number().int().min(1).default(1)
+  })
 })
--- src/pages/catalog/page.tsx
+++ src/pages/catalog/page.tsx
@@ -1,16 +1,62 @@
-import { Check } from 'lucide-react'
+import clsx from 'clsx'
+import { ArrowLeft, ArrowRight, CheckIcon } from 'lucide-react'
+import { ROUTES } from '@/shared/constants/routes'
+import { Button } from '@/shared/ui'
 import { Checkbox } from '@/shared/ui/Checkbox'
+import { usePagination } from './hooks/usePagination'
 import styles from './page.module.css'
+// import { useLoaderData } from '@tanstack/react-router'
 
 export function CatalogPage() {
+  const { currentPage, nextPages, prevPages, next, prev, setPage } =
+    usePagination(ROUTES.CATALOG)
+  // const { sneakers, brands } = useLoaderData({ from: ROUTES.CATALOG })
+
   return (
     <div className={styles.catalog}>
-      <Checkbox>
-        <Checkbox.Box>
-          <Check size={18} />
-        </Checkbox.Box>
-        <Checkbox.Label>Label</Checkbox.Label>
-      </Checkbox>
+      <aside className={styles.sidebar}>
+        <Checkbox>
+          <Checkbox.Box>
+            <CheckIcon size={18} />
+          </Checkbox.Box>
+          <Checkbox.Label>Label</Checkbox.Label>
+        </Checkbox>
+      </aside>
+      <div className={styles.pagination}>
+        <Button
+          disabled={currentPage === 1}
+          onClick={prev}
+          className={styles.navigationButton}
+        >
+          <ArrowLeft />
+        </Button>
+        <div className={styles.pages}>
+          {prevPages.map(page => (
+            <span
+              key={page}
+              className={styles.page}
+              onClick={() => setPage(page)}
+            >
+              {page}
+            </span>
+          ))}
+          <span className={clsx(styles.page, styles.currentPage)}>
+            {currentPage}
+          </span>
+          {nextPages.map(page => (
+            <span
+              key={page}
+              className={styles.page}
+              onClick={() => setPage(page)}
+            >
+              {page}
+            </span>
+          ))}
+        </div>
+        <Button onClick={next} className={styles.navigationButton}>
+          <ArrowRight />
+        </Button>
+      </div>
     </div>
   )
 }
